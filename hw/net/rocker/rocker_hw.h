7a07aa39dd3fadbd0e101648c49143a66469bda8
Neil Horman <nhorman@tuxdriver.com>
2014-04-24 14:24:41 -0400

    rocker: add dma register macros, and structures
    Translate the rocker spec to header file definitions.  This patch includes the
    interrupt status register set, as well as the dma/completion/command/event
    descriptor ring entry structures.
    
    Note also that this patch updates the spec to clarify the alignment needs to
    individual structures.  The padding in the structures was intended to ensure
    that each instance ended on an 8 byte boundary.  In an effort to be more
    explicit, the documentation has been updated to state that each structure
    instance in a dma buffer must be 8 byte aligned, and is implemented in the
    header file using an __atttribute__((aligned (8))) directive.
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>
    CC: sfeldma@cumulusnetworks.com
    CC: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: Scott Feldman <sfeldma@cumulusnetworks.com>

--- hw/net/rocker/rocker_hw.h
+++ hw/net/rocker/rocker_hw.h
@@ -29,4 +29,66 @@
 
 #define ROCKER_PCI_BAR0_SIZE		0x1000
 
+/*
+ * Rocker IRQ register offsets
+ */
+#define ROCKER_IRQ_MASK	0x0010
+#define ROCKER_IRQ_STAT	0x0014
+
+/*
+ * Rocker IRQ status bits
+ */
+#define IRQ_LINK		(1 << 0)
+#define IRQ_TX_DMA_DONE		(1 << 1)
+#define IRQ_RX_DMA_DONE		(1 << 2)
+#define IRQ_CMD_DMA_DONe	(1 << 3)
+#define IRQ_EVENT_DMA_DONE	(1 << 5)
+
+/*
+ * Rocker DMA ring register offsets
+ */
+#define DMA_DESC_ADDR(x)		(0x0100 + (x) * 16)
+#define DMA_COMP_ADDR(x)		(0x0108 + (x) * 16)
+#define DMA_DESC_SIZE(x)		(0x0110 + (x) * 16)
+#define DMA_DESC_HEAD(x)		(0x0114 + (x) * 16)
+#define DMA_DESC_TAIL(x)		(0x0118 + (x) * 16)
+#define DMA_DESC_CTRL(x)		(0x011c + (x) * 16)
+
+#define ROCKER_DMA_RING_REG_SET(name, index) \
+enum {\
+	name ## _DMA_DESC_ADDR = DMA_DESC_ADDR(index),\
+	name ## _DMA_COMP_ADDR = DMA_COMP_ADDR(index),\
+	name ## _DMA_DESC_SIZE = DMA_DESC_SIZE(index),\
+	name ## _DMA_DESC_HEAD = DMA_DESC_HEAD(index),\
+	name ## _DMA_DESC_TAIL = DMA_DESC_TAIL(index),\
+	name ## _DMA_DESC_CTRL = DMA_DESC_CTRL(index),\
+}
+
+ROCKER_DMA_RING_REG_SET(TX, 0);
+ROCKER_DMA_RING_REG_SET(RX, 1);
+ROCKER_DMA_RING_REG_SET(CMD, 2);
+ROCKER_DMA_RING_REG_SET(EVENT, 3);
+
+/*
+ * Rocker DMA Descriptor and completion structs
+ */
+struct rocker_dma_tlv {
+	uint32_t type;
+	uint16_t len;
+	uint8_t val[0] __attribute__((aligned(8)));
+} __attribute__((packed, aligned (8)));
+
+struct rocker_dma_desc {
+	uint64_t buf_addr;
+	uint64_t cookie;
+	uint16_t buf_size;
+} __attribute__((packed, aligned (8)));
+
+struct rocker_comp_desc {
+	struct rocker_dma_desc desc;
+	uint64_t cookie_gen;
+	uint16_t comp_written;
+	uint16_t comp_status;
+} __attribute__((packed, aligned(8)));
+
 #endif /* _ROCKER_HW_ */
