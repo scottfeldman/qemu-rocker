c84594b8af26813c091079c18d237b1a052b3c20
Scott Feldman <sfeldma@cumulusnetworks.com>
2014-05-05 21:05:33 -0700

    Add dma.c implemention for desc ring operations
    Add a new dma.c for DMA desc ring implementation.  The consolidates all desc
    ring opertions into one place and hides the desc ring implemention details from
    client code.  Add some notes to reg_guide about ring empty, ring full
    conditions.  Add callback for Tx and DMA rings to process descs.  (Rx and Event
    rings don't need a consumer callbacks into driver since the driver is the
    producer of those descs).  Add sanity check for ring overrun, underrun
    conditions as well as alignment issues and DPRINTF errs.  On HEAD write,
    automatically process descriptors and generate interrupt for desc ring if work
    was done (and interrupt for that ring is unmasked).
    
    I believe at this point we have what we need to process Tx and CMD rings.  Just
    need the matching driver code and TLV parses for descs.
    
    Signed-off-by: Scott Feldman <sfeldma@cumulusnetworks.com>

---
+++ hw/net/rocker/rocker_desc.c
@@ -0,0 +1,222 @@
+/*
+ * QEMU rocker switch emulation - Descriptor ring support
+ *
+ * Copyright (c) 2014 Scott Feldman <sfeldma@cumulusnetworks.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#include "hw/hw.h"
+#include "hw/pci/pci.h"
+
+#include "rocker.h"
+#include "rocker_hw.h"
+#include "rocker_desc.h"
+
+struct desc_ring {
+    union {
+        hwaddr addr;
+        struct rocker_desc desc[0];
+    } base;
+    uint32_t size;
+    uint32_t head;
+    uint32_t tail;
+    uint32_t ctrl;
+    struct rocker *rocker;
+    PCIDevice *dev;
+    struct rocker_desc *backing;
+    int index;
+    desc_ring_consume *consume;
+};
+
+bool desc_ring_empty(struct desc_ring *ring)
+{
+    return ring->head == ring->tail;
+}
+
+bool desc_ring_full(struct desc_ring *ring)
+{
+    return ((ring->head + 1) % ring->size) == ring->tail;
+}
+
+bool desc_ring_set_base_addr(struct desc_ring *ring, uint64_t base_addr)
+{
+    if (base_addr & 0x7) {
+        DPRINTF("ERROR: ring[%d] desc base addr (0x%lx) not 8-byte aligned\n",
+                ring->index, base_addr);
+        return false;
+    }
+
+    ring->base.addr = base_addr;
+
+    return true;
+}
+
+uint64_t desc_ring_get_base_addr(struct desc_ring *ring)
+{
+    return ring->base.addr;
+}
+
+bool desc_ring_set_size(struct desc_ring *ring, uint32_t size)
+{
+    if (size < 2 || size > 0x10000 || (size & (size - 1))) {
+        DPRINTF("ERROR: ring[%d] size (%d) not a power of 2 or in range [2, 64K]\n",
+                ring->index, size);
+        return false;
+    }
+
+    ring->size = size;
+    ring->head = ring->tail = 0;
+
+    ring->backing = g_realloc(ring->backing, size * sizeof(struct rocker_desc));
+    if (!ring->backing)
+        return false;
+
+    return true;
+}
+
+uint32_t desc_ring_get_size(struct desc_ring *ring)
+{
+    return ring->size;
+}
+
+static struct rocker_desc *desc_read(struct desc_ring *ring, uint32_t index)
+{
+    struct rocker_desc *desc = &ring->backing[index];
+    hwaddr addr = (hwaddr)&ring->base.desc[index];
+
+    pci_dma_read(ring->dev, addr, desc, sizeof(*desc));
+
+    return desc;
+}
+
+static void desc_write(struct desc_ring *ring, uint32_t index)
+{
+    struct rocker_desc *desc = &ring->backing[index];
+    hwaddr addr = (hwaddr)&ring->base.desc[index];
+
+    pci_dma_write(ring->dev, addr, desc, sizeof(*desc));
+}
+
+struct rocker_desc *desc_ring_fetch_desc(struct desc_ring *ring)
+{
+    if (desc_ring_empty(ring))
+        return NULL;
+
+    return desc_read(ring, ring->tail);
+}
+
+void desc_ring_post_desc(struct desc_ring *ring, struct rocker_desc *desc,
+                         int status)
+{
+    uint16_t comp_status = 0x8000 | (uint16_t)status;
+
+    if (desc_ring_empty(ring)) {
+        DPRINTF("ERROR: ring[%d] trying to post desc to empty ring\n",
+                ring->index);
+        return;
+    }
+
+    desc->comp_status = cpu_to_le16(comp_status);
+    desc_write(ring, ring->tail);
+    ring->tail = (ring->tail + 1) % ring->size;
+}
+
+static int ring_pump(struct desc_ring *ring)
+{
+    struct rocker_desc *desc;
+    int status, consumed = 0;
+
+    /* If the ring has a consumer, call consumer for each
+     * desc starting at tail and stopping when tail reaches
+     * head (the empty ring condition).
+     */
+
+    if (ring->consume) {
+        while (ring->head != ring->tail) {
+            desc = desc_read(ring, ring->tail);
+            status  = ring->consume(ring->rocker, desc);
+            desc_ring_post_desc(ring, desc, status);
+            consumed++;
+        }
+    }
+
+    return consumed;
+}
+
+int desc_ring_set_head(struct desc_ring *ring, uint32_t new)
+{
+    uint32_t tail = ring->tail;
+    uint32_t head = ring->head;
+
+    if (new >= ring->size) {
+        DPRINTF("ERROR: trying to set head (%d) past ring[%d] size (%d) \n",
+                new, ring->index, ring->size);
+        return 0;
+    }
+
+    if (((head < tail) && ((new >= tail) || (new < head))) ||
+        ((head > tail) && ((new >= tail) && (new < head)))) {
+        DPRINTF("ERROR: trying to wrap ring[%d] (head %d, tail %d, new head %d)\n",
+                ring->index, head, tail, new);
+        return 0;
+    }
+
+    ring->head = new;
+
+    return ring_pump(ring);
+}
+
+uint32_t desc_ring_get_head(struct desc_ring *ring)
+{
+    return ring->head;
+}
+
+uint32_t desc_ring_get_tail(struct desc_ring *ring)
+{
+    return ring->tail;
+}
+
+bool desc_ring_set_ctrl(struct desc_ring *ring, uint32_t new)
+{
+    ring->ctrl = new;
+    return true;
+}
+
+uint32_t desc_ring_get_ctrl(struct desc_ring *ring)
+{
+    return ring->ctrl;
+}
+
+struct desc_ring *desc_ring_alloc(struct rocker *r,
+                                  int index, PCIDevice *dev,
+                                  desc_ring_consume *consume)
+{
+    struct desc_ring *ring;
+
+    ring = g_malloc0(sizeof(struct desc_ring));
+    if (!ring)
+        return NULL;
+
+    ring->rocker = r;
+    ring->dev = dev;
+    ring->consume = consume;
+    ring->index = index;
+
+    return ring;
+}
+
+void desc_ring_free(struct desc_ring *ring)
+{
+    if (ring->backing)
+        g_free(ring->backing);
+    g_free(ring);
+}
---
+++ hw/net/rocker/rocker_desc.h
@@ -0,0 +1,49 @@
+/*
+ * QEMU rocker switch emulation - Descriptor ring support
+ *
+ * Copyright (c) 2014 Scott Feldman <sfeldma@cumulusnetworks.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+
+#ifndef _ROCKER_DESC_H_
+#define _ROCKER_DESC_H_
+
+#include "rocker_hw.h"
+
+struct rocker;
+struct desc_ring;
+
+typedef int (desc_ring_consume)(struct rocker *r, struct rocker_desc *desc);
+
+bool desc_ring_empty(struct desc_ring *ring);
+bool desc_ring_full(struct desc_ring *ring);
+bool desc_ring_set_base_addr(struct desc_ring *ring, uint64_t base_addr);
+uint64_t desc_ring_get_base_addr(struct desc_ring *ring);
+bool desc_ring_set_size(struct desc_ring *ring, uint32_t size);
+uint32_t desc_ring_get_size(struct desc_ring *ring);
+int desc_ring_set_head(struct desc_ring *ring, uint32_t new);
+uint32_t desc_ring_get_head(struct desc_ring *ring);
+uint32_t desc_ring_get_tail(struct desc_ring *ring);
+bool desc_ring_set_ctrl(struct desc_ring *ring, uint32_t new);
+uint32_t desc_ring_get_ctrl(struct desc_ring *ring);
+
+struct rocker_desc *desc_ring_fetch_desc(struct desc_ring *ring);
+void desc_ring_post_desc(struct desc_ring *ring, struct rocker_desc *desc,
+                         int status);
+
+struct desc_ring *desc_ring_alloc(struct rocker *r,
+                                  int index, PCIDevice *dev,
+                                  desc_ring_consume *consume);
+void desc_ring_free(struct desc_ring *ring);
+
+#endif
